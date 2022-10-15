5d51d213eddf6e74e58dea2e2bf63f9d8db60287
Bruno Groth <brunom.groth@gmail.com>
2022-10-15 20:01:35 -0300

    Shopping list adding and marking as Done

--- ShoppingList/scripts.js
+++ ShoppingList/scripts.js
@@ -1,6 +1,44 @@
-var itemName = document.getElementById("itemName");
+var input = document.getElementById("itemName");
 var button = document.getElementById("addItem");
+var list = document.getElementById("list");
 
-button.addEventListener("click", function(){
-    console.log("ok");
-});
+
+// Items Creation
+
+function inputLength(){
+    return input.value.length;
+}
+
+function addItem(){
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(input.value));
+    li.classList.add("listedItem");
+    list.appendChild(li);
+    input.value = "";
+} 
+
+function addItemClick(){
+    if(inputLength() > 0){
+        addItem()
+    }
+}
+
+function addItemEnter(event){
+    if(inputLength() > 0 && event.key === "Enter"){
+        addItem();
+    }
+}
+
+button.addEventListener("click", addItemClick);
+input.addEventListener("keypress", addItemEnter);
+
+// Marking item as done
+
+function MarkAsDone(item){
+    item.addEventListener("click", function(){
+        item.classList.toggle("done");
+    });
+}
+var listedItem = document.querySelectorAll(".listedItem");
+
+listedItem.forEach(MarkAsDone);
